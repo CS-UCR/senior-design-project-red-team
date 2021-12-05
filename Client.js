b97d970adc44e5afde6526ad294ad5dbaf83f29d
Jeremy Cartwright <zycron83@gmail.com>
2021-12-05 12:21:20 -0800

    Up to Milestone 2 Demo

--- Client.js
+++ Client.js
@@ -1,7 +1,7 @@
 const hostname = '127.0.0.1'
 const port = '8080'
 
-const flight_parameters = ['AILERON POSITION LH', 'AILERON POSITION RH', 'CORRECTED ANGLE OF ATTACK', 'BARO CORRECT ALTITUDE LSP', 'BARO CORRECT ALTITUDE LSP', 'COMPUTED AIRSPEED LSP', 'SELECTED COURSE', 'DRIFT ANGLE', 'ELEVATOR POSITION LEFT', 'ELEVATOR POSITION RIGHT', 'T.E.FLAP POSITION', 'GLIDESLOPE DEVIATION', 'SELECTED HEADING', 'SELECTED AIRSPEED', 'LOCALIZER DEVIATION', 'N1 COMMAND LSP', 'TOTAL PRESSURE LSP', 'PITCH ANGLE LSP', 'ROLL ANGLE LSP', 'RUDDER POSITION', 'TRUE HEADING LSP', 'VERTICAL ACCELERATION', 'WIND SPEED', 'AP FD STATUS', 'GEARS L & R DOWN LOCKED', 'TCAS LSP', 'WEIGHT ON WHEELS']
+// const flight_parameters = ['AILERON POSITION LH', 'AILERON POSITION RH', 'CORRECTED ANGLE OF ATTACK', 'BARO CORRECT ALTITUDE LSP', 'BARO CORRECT ALTITUDE LSP', 'COMPUTED AIRSPEED LSP', 'SELECTED COURSE', 'DRIFT ANGLE', 'ELEVATOR POSITION LEFT', 'ELEVATOR POSITION RIGHT', 'T.E.FLAP POSITION', 'GLIDESLOPE DEVIATION', 'SELECTED HEADING', 'SELECTED AIRSPEED', 'LOCALIZER DEVIATION', 'N1 COMMAND LSP', 'TOTAL PRESSURE LSP', 'PITCH ANGLE LSP', 'ROLL ANGLE LSP', 'RUDDER POSITION', 'TRUE HEADING LSP', 'VERTICAL ACCELERATION', 'WIND SPEED', 'AP FD STATUS', 'GEARS L & R DOWN LOCKED', 'TCAS LSP', 'WEIGHT ON WHEELS']
 
 function init() {
     fetch("http://" + hostname + ":" + port + "/" + ['flights'].join('/'))
@@ -16,25 +16,48 @@ function init() {
             })
         })
         .catch(err => console.error(err));
-    var par1 = document.getElementById('parameter-1')
-    var par2 = document.getElementById('parameter-2')
-    flight_parameters.forEach(x => {
-        var o1 = document.createElement('option')
-        var o2 = document.createElement('option')
-        o1.innerHTML = x
-        o2.innerHTML = x
-        par1.appendChild(o1)
-        par2.appendChild(o2)
-    })
+    fetch("http://" + hostname + ":" + port + "/" + ['parameters'].join('/'))
+        .then(response => response.json())
+        .then(flight_parameters => {
+            var par1 = document.getElementById('parameter-1')
+            var par2 = document.getElementById('parameter-2')
+            flight_parameters.forEach(x => {
+                var o1 = document.createElement('option')
+                var o2 = document.createElement('option')
+                o1.innerHTML = x
+                o2.innerHTML = x
+                par1.appendChild(o1)
+                par2.appendChild(o2)
+            })
+        })
+        .catch(err => console.error(err));
 }
 
-const chart = document.getElementById('test-chart');
+function goto1() {
+    refresh_chart = two_parameter_chart;
+    document.getElementById('parameter-2').disabled = false;
+    document.getElementById('add-button').hidden = true;
+}
+function goto2() {
+    refresh_chart = dtr_chart;
+    document.getElementById('parameter-2').disabled = true;
+    document.getElementById('add-button').hidden = true;
+}
+function goto3() {
+    refresh_chart = () => time_series(true);
+    document.getElementById('parameter-2').disabled = true;
+    document.getElementById('add-button').hidden = false;
 
-function refresh_chart() {
+}
+
+var refresh_chart = two_parameter_chart;
+
+function two_parameter_chart() {
+    const chart = document.getElementById('test-chart');
     var flight = document.getElementById('file-select').value
     var par1 = document.getElementById('parameter-1').value
     var par2 = document.getElementById('parameter-2').value
-    fetch("http://" + hostname + ":" + port + "/" + ['flight', flight, par1, par2].join('/'))
+    fetch("http://" + hostname + ":" + port + "/" + ['two-parameter', flight, par1, par2].join('/'))
         .then(response => response.json())
         .then(data => {
             if (par1 == par2) {
@@ -53,6 +76,53 @@ function refresh_chart() {
             }
             console.log(data);
             Plotly.newPlot(chart, [plot_data], {
+                margin: { t: 0 }
+            })
+        })
+        .catch(err => console.error(err))
+}
+
+function dtr_chart() {
+    const chart = document.getElementById('test-chart');
+    var flight = document.getElementById('file-select').value
+    var par1 = document.getElementById('parameter-1').value
+    fetch("http://" + hostname + ":" + port + "/" + ['dtr', flight, par1].join('/'))
+        .then(response => response.json())
+        .then(data => {
+            plot_data = {
+                x: data.x.map((x) => -x + data.x[data.x.length - 1]),
+                y: data.y,
+                mode: 'markers',
+                type: 'scatter'
+            };
+            console.log(data);
+            Plotly.newPlot(chart, [plot_data], {
+                margin: { t: 0 },
+                xaxis: { autorange: 'reversed' }
+            })
+        })
+        .catch(err => console.error(err))
+}
+
+var data_bank = [];
+
+function time_series(refresh) {
+    const chart = document.getElementById('test-chart');
+    var flight = document.getElementById('file-select').value
+    var par1 = document.getElementById('parameter-1').value
+    fetch("http://" + hostname + ":" + port + "/" + ['time-series', flight, par1].join('/'))
+        .then(response => response.json())
+        .then(data => {
+            if (refresh) data_bank = [];
+            // else data.y = integral(data.y);
+            data_bank.push({
+                x: data.x,
+                y: data.y,
+                mode: 'markers',
+                type: 'scatter'
+            });
+            console.log(data);
+            Plotly.newPlot(chart, data_bank, {
                 margin: { t: 0 }
             })
         })
--- Server.js
+++ Server.js
@@ -1,17 +1,27 @@
 const http = require('http');
 const fs = require('fs');
-const csv = require('csv-parse/sync');
+// const csv = require('csv-parse/sync'); // NodeJS 16
+const csv = require('csv-parse/lib/sync.js'); // NodeJS 12
 
-const flight_data_dir = 'flight-data/';
+const flight_data_dir = 'true-flight-data/';
 
 const getFlights = () => fs.readdirSync(flight_data_dir);
+const getParameters = () => {
+    try {
+        return csv_cols.keys();
+    } catch {
+        return fs.readFileSync(flight_data_dir + getFlights()[0]).toString().split('\n')[0].split(',');
+    }
+}
 
-var csv_cols;
+var csv_cols = {};
 var curr_csv = '';
+
 
 function loadCSV(filename) {
     let data = fs.readFileSync(flight_data_dir + filename);
-    let csv_data = csv.parse(data);
+    // let csv_data = csv.parse(data); // NodeJS 16
+    let csv_data = csv(data); // NodeJS 12
     csv_cols = {};
     curr_csv = filename;
     for (let col_num = 0; col_num < csv_data[0].length; col_num++) {
@@ -22,6 +32,68 @@ function loadCSV(filename) {
             csv_cols[col_name].push(val.includes('.') ? parseFloat(val) : parseInt(val));
         }
     }
+}
+
+// function DTR_graph_data(parameter) { // for reduced flight-data files
+    
+// }
+
+function transpose(ls) {
+    var result = [];
+    for (var i = 0; i < ls[0].length; i++) {
+        var tmp = [];
+        for (var j = 0; j < ls.length; j++) {
+            tmp.push(ls[j][i]);
+        }
+        result.push(tmp);
+    }
+    return result;
+}
+
+function range(start, end) {
+    res = [];
+    for (let i = start; i < end; i++) {
+        res.push(i);
+    }
+    return res;
+}
+
+function integral(list) {
+    res = [1];
+    sum = 1;
+    list.forEach((x) => {
+        sum += x;
+        res.push(sum);
+    })
+    return res;
+}
+
+var parameter_mean = [];
+
+function calculate_mean(col_name) {
+    buckets = [];
+    let insert_bucket = (x, y) => {
+        for (let bucket in buckets) {
+            if (x == bucket[0]) {
+                bucket[1].push(y);
+                return;
+            }
+        }
+        buckets.push([x, [y]]);
+
+    }
+    for (let filename of getFlights()) {
+        if (filename.split('.')[1] == 'pkl') continue;
+        loadCSV(filename);
+        let data = {
+            x: integral(csv_cols['GROUND SPEED LSP']).map((x) => (x - csv_cols['GROUND SPEED LSP'][csv_cols['GROUND SPEED LSP'].length - 1]) / 5280),
+            y: csv_cols[col_name]
+        };
+        for (let i = 0; i < data.x.length; i++) {
+            insert_bucket(data.x[i], data.y[i]);
+        }
+    }
+    
 }
 
 const server = http.createServer((req, res) => {
@@ -44,15 +116,36 @@ const server = http.createServer((req, res) => {
                 );
         console.log(requested_data)
         switch (requested_data[0]) {
+            case 'parameters': 
+                // console.log(getParameters())
+                res.write(JSON.stringify(getParameters()))
+                break;
             case 'flights':
                 res.write(JSON.stringify(getFlights()));
                 break;
-            case 'flight':
+            case 'two-parameter':
                 if (requested_data[1] != curr_csv)
                     loadCSV(requested_data[1]);
                 res.write(JSON.stringify({
                     x: csv_cols[requested_data[2]],
                     y: csv_cols[requested_data[3]]
+                }));
+                break;
+            case 'dtr':
+                if (requested_data[1] != curr_csv)
+                    loadCSV(requested_data[1]);
+                // console.log(integral(csv_cols['GROUND SPEED LSP']))
+                res.write(JSON.stringify({
+                    x: integral(csv_cols['GROUND SPEED LSP']).map((x) => (x - csv_cols['GROUND SPEED LSP'][csv_cols['GROUND SPEED LSP'].length - 1]) / 5280),
+                    y: csv_cols[requested_data[2]]
+                }));
+                break;
+            case 'time-series':
+                if (requested_data[1] != curr_csv)
+                    loadCSV(requested_data[1]);
+                res.write(JSON.stringify({
+                    x: range(0, csv_cols[requested_data[2]].length),
+                    y: csv_cols[requested_data[2]]
                 }));
                 break;
         }
