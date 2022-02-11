4a0088452ce87f6158c7188d686e10eb4b1ae759
David Gutierrez <davidgutierrez@Davids-MBP.lan>
2022-02-11 11:50:54 -0800

    Selecting Anomalies

--- Client.js
+++ Client.js
@@ -51,13 +51,15 @@ function goto3() {
 }
 
 var refresh_chart = two_parameter_chart;
-
+var type_of_graph = ''
 function two_parameter_chart() {
     const chart = document.getElementById('test-chart');
     var flight = document.getElementById('file-select').value
     var par1 = document.getElementById('parameter-1').value
     var par2 = document.getElementById('parameter-2').value
-    fetch("http://" + hostname + ":" + port + "/" + ['two-parameter', flight, par1, par2].join('/'))
+    type_of_graph = 'two-parameter'
+
+    fetch("http://" + hostname + ":" + port + "/" + [type_of_graph, flight, par1, par2].join('/'))
         .then(response => response.json())
         .then(data => {
             if (par1 == par2) {
@@ -66,6 +68,7 @@ function two_parameter_chart() {
                     y: data.y,
                     type: 'histogram'
                 };
+                
             } else {
                 plot_data = {
                     x: data.x,
@@ -75,17 +78,145 @@ function two_parameter_chart() {
                 };
             }
             console.log(data);
-            Plotly.newPlot(chart, [plot_data], {
-                margin: { t: 0 }
-            })
+            config  = removeEvents()
+            Plotly.newPlot(chart,[plot_data], {
+                margin: { t: 0 },
+                
+                
+            },config)
+            selectDisplay()
+
+
+            
+
         })
         .catch(err => console.error(err))
 }
+function removeEvents(){
+    var config = {
+        modeBarButtonsToRemove:[
+            'lasso2d'
+        ]
+    }
+    return config
+}
+function selectDisplay(){
+    console.log('clicked')
+    const chart = document.getElementById('test-chart');
+
+    var x_start = 0
+    var x_end = 0
+    var y_start = 0
+    var y_end = 0
+
+    var x_values = []
+    var y_values = []
+    chart.on('plotly_selected',
+        function(data){
+            console.log(data)
+  
+            x_start =  data.range.x[0]
+            x_end   =  data.range.x[1]
+            y_start =  data.range.y[0]
+            y_end   =  data.range.y[1] 
+
+            for(var i = 0; i < data.points.length; i++){
+                x_values.push(data.points[i].x)
+                y_values.push(data.points[i].y)
+            }
+            // console.log(x_values,y_values)
+            selected_values = {
+                x: x_values,
+                y: y_values,
+                xaxis: {range: [x_start,x_end]},
+                yaxis: {range: [y_start,y_end]}
+
+            }
+            Anomaly_list.push(selected_values)
+            console.log(Anomaly_list)
+            Log_Display()
+            x_values = []
+            y_values = []
+
+        })
+       
+
+}
+
+
+// FLap slate path high speed high 
+var Anomaly_list = []
+function Log_Display(){
+    
+    var flight = document.getElementById('file-select').value
+    var par1 = document.getElementById('parameter-1').value
+    var par2 = document.getElementById('parameter-2').value
+
+
+    var s_flight = document.getElementById('fid').value = String(flight)
+    var type = document.getElementById('gid').value = type_of_graph
+    var par = document.getElementById('pid').value = par1
+    var parr =  document.getElementById('pid2').value = par2
+ 
+    // var mylist = document.getElementById('class-id').value;
+   
+
+
+    var graph_values 
+    if(Anomaly_list.length <= 1){
+        graph_values = Anomaly_list[0]
+    }
+    else{
+        graph_values  = Anomaly_list[Anomaly_list.length-1]
+    }
+    console.log(graph_values)
+    // console.log(v)
+    saveForm(graph_values,s_flight,type,par,parr)
+}
+
+function openForm(){
+    document.getElementById('logForm').style.display = 'block'
+}
+// var mylist = document.getElementById("class-id")
+var v_test = ''
+function type_Anomally(){
+    var mylist = document.getElementById("class-id")
+    v_test = document.getElementById("demo").value = mylist.options[mylist.selectedIndex].text
+}
+function saveForm(graph_values,s_flight,type,par,parr){
+
+    var save_btn = document.getElementById('save_btn')
+    // var input =  mylist.options[mylist.selectedIndex].text
+    // console.log(input)
+    // console.log(v_test)
+    save_btn.onclick = function(){
+        var upload = {
+            Points: graph_values,
+            Flight: s_flight,
+            Type: type,
+            Parameters: [par,parr],
+            Anomaly: v_test
+
+        }
+        console.log(upload)
+        
+    }
+
+}
+function closeForm(){
+    document.getElementById('logForm').style.display = 'none'
+    document.getElementById('fid').value = ''
+    document.getElementById('gid').value = ''
+    document.getElementById('pid').value = ''
+    document.getElementById('pid2').value = ''
+}
+
 
 function dtr_chart() {
     const chart = document.getElementById('test-chart');
     var flight = document.getElementById('file-select').value
     var par1 = document.getElementById('parameter-1').value
+    var zoom_btn  = document.getElementById('zoom-btn')
     fetch("http://" + hostname + ":" + port + "/" + ['dtr', flight, par1].join('/'))
         .then(response => response.json())
         .then(data => {
@@ -100,6 +231,7 @@ function dtr_chart() {
                 margin: { t: 0 },
                 xaxis: { autorange: 'reversed' }
             })
+            selectDisplay()
         })
         .catch(err => console.error(err))
 }
@@ -110,6 +242,7 @@ function time_series(refresh) {
     const chart = document.getElementById('test-chart');
     var flight = document.getElementById('file-select').value
     var par1 = document.getElementById('parameter-1').value
+    var zoom_btn  = document.getElementById('zoom-btn')
     fetch("http://" + hostname + ":" + port + "/" + ['time-series', flight, par1].join('/'))
         .then(response => response.json())
         .then(data => {
@@ -125,6 +258,8 @@ function time_series(refresh) {
             Plotly.newPlot(chart, data_bank, {
                 margin: { t: 0 }
             })
+            selectDisplay()
+
         })
         .catch(err => console.error(err))
 }
